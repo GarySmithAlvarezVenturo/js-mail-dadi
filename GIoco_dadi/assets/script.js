54f7b147649c6996d91366bab3f4f70f9ccc54aa
GarySmithAlvarezVenturo <Garyalvarez@outlook.it>
2023-04-18 16:15:28 +0200

    fix dadi

--- GIoco_dadi/assets/script.js
+++ GIoco_dadi/assets/script.js
@@ -1,5 +1,47 @@
-/* Chiedi all’utente la sua email,
-controlla che sia nella lista di chi può accedere,
-stampa un messaggio appropriato sull’esito del controllo. */
+/*
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+*/
 
-const requestMail = prompt("Inserisci qui la tua email");
+const eleBtnPlay = document.querySelector('#btn-play');
+const eleScoreUser = document.querySelector('#score-player');
+const eleScoreComputer = document.querySelector('#score-computer');
+const eleOutput = document.querySelector('#output');
+
+eleBtnPlay.addEventListener('click',
+	function() {
+
+		// generare primo numero random e metterlo nella variabile numUser
+		const numUser = Math.floor(Math.random() * 6) + 1;
+
+		// generare il secondo numero random e metterlo nella variabile numComputer
+		const numComputer = Math.floor(Math.random() * 6) + 1;
+
+		/* SE numUser > numComputer
+				ha vinto il giocatore
+			ALTRIMENTI numComputer > numUser
+				ha vinto il computer
+			ALTRIMENTI
+				pareggio
+			FINE SE */
+
+		let message;
+		if (numUser > numComputer) {
+			message = 'Hai vinto';
+		} else if (numComputer > numUser) {
+			message = 'Hai perso';
+		} else {
+			message = 'Avete pareggiato';
+		}
+
+		eleOutput.innerHTML = message;
+		console.log(message);
+
+		eleScoreUser.innerHTML = numUser;
+		eleScoreComputer.innerHTML = numComputer;
+		console.log(numUser, numComputer);
+		// condizione ? se vero : se falso
+		eleScoreUser.className = message == 'Hai vinto' ? 'green' : 'red';
+		eleScoreComputer.className = message == 'Hai vinto' ? 'red' : 'green';
+	}
+);
